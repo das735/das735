8345f19584d54adc1c9232d7d8f8ba94fca75cf2
das735 <cova456@outlook.com>
2024-08-09 21:38:07 +0500

    Update module_3.4(1).py

--- module_3.4(1).py
+++ module_3.4(1).py
@@ -1,12 +1,13 @@
-
 def single_root_words (root_word , *other_words) :
     same_words = [ ]
-    for i in range(len(*other_words)):
-        same_words is other_words.count(root_word)
-    print(other_words)
+    for i in other_words:
+        if root_word.lower().count(i.lower()) or i.lower().count(root_word.lower()):
+            same_words.append(i)
+    return same_words
 
 
 result1 = single_root_words ( 'rich' , 'richiest' , 'orichalcum' , 'cheers' , 'richies' )
 result2 = single_root_words ( 'Disablement', 'Able' , 'Mable' , 'Disable' , 'Bagel' )
 print ( result1 )
 print ( result2 )
+
